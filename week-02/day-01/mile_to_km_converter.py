1b06283e9b646c492b01d6e739d15cb089967444
Kucika14 <mate.akos@hotmail.com>
2017-09-11 15:15:36 +0200

    task 15 finihsed

--- week-02/day-01/mile_to_km_converter.py
+++ week-02/day-01/mile_to_km_converter.py
@@ -1,2 +1,5 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
+print('Please enter a number what you need to convert')
+user_number = int(input())
+print("this is " + str(user_number*0.62) + " mile" )
