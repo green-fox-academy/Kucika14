1da2ddb9fe3708322f3633f56f79cc12662d3182
Kucika14 <mate.akos@hotmail.com>
2017-09-11 16:26:39 +0200

    task 20 finihsed

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,7 @@
 # Write a program that asks for two numbers and prints the bigger one
+number = int(input('Pls enter a number: '))
+number2 = int(input('Pls enter another one: '))
+if number <= number2:
+    print(number2)
+else:
+    print(number)
