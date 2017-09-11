c5585818be059e82c042af022550bc86c2ccf66d
Kucika14 <mate.akos@hotmail.com>
2017-09-11 15:58:42 +0200

    task 17 finihsed

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,12 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+print('Pls enter five number')
+num1=int(input())
+num2=int(input())
+num3=int(input())
+num4=int(input())
+num5=int(input())
+
+print("Your sum number is " + str(num1+num2+num3+num4+num5))
+print("Your average number is " + str((num1+num2+num3+num4+num5) /5))
