886468ee3a1a5b2cc0ca150b56457c4207330a70
Kucika14 <mate.akos@hotmail.com>
2017-09-11 13:54:39 +0200

    task 10 finihsed

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,12 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+i = "Akos"
+age = 26
+height = 1.75
+married = True
+
+print(i)
+print(int(age))
+print(height)
+print(married)
