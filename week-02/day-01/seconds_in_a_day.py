1c509bd691e31e10dae3f508bf068eb8d0eacf37
Kucika14 <mate.akos@hotmail.com>
2017-09-11 14:56:10 +0200

    task 13 finihsed

--- week-02/day-01/seconds_in_a_day.py
+++ week-02/day-01/seconds_in_a_day.py
@@ -4,3 +4,6 @@ current_seconds = 42;
 
 # Write a program that prints the remaining seconds (as an integer) from a
 # day if the current time is represented bt the variables
+all_seconds = 24*60*60
+past_seconds = ((14*60*60) + (34*60) + 42)
+print(str(all_seconds % past_seconds) + " seconds remaining from the day!" ) 
