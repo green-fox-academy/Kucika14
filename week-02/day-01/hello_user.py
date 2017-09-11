d1ae133662abcaa5dd3b6e5b2349f07765b25f2e
Kucika14 <mate.akos@hotmail.com>
2017-09-11 15:00:48 +0200

    task 14 finihsed

--- week-02/day-01/hello_user.py
+++ week-02/day-01/hello_user.py
@@ -1,3 +1,4 @@
 # Modify this program to greet the user instead of the World!
 # The program should ask for the name of the user
-print("Hello, World!")
+print("Hello my dear! What's your name?")
+what_is_your_name = input()
