c6b9298886011bcfde430124b055375996605ccd
Kucika14 <mate.akos@hotmail.com>
2017-09-11 15:39:54 +0200

    task 1 finihsed

--- week-02/day-01/animals_and_legs.py
+++ week-02/day-01/animals_and_legs.py
@@ -1,4 +1,9 @@
 # Write a program that asks for two integers
 # The first represents the number of chickens the farmer has
-# The seconf represents the number of pigs the farmer has
+# The second represents the number of pigs the farmer has
 # It should print how many legs all the animals have
+print('How many chicken you have?')
+chicken = int(input())
+print('How many pig you have?')
+pig = int(input())
+print('Than you have ' + str(chicken + pig) + ' animals and they have ' + str(chicken*2 + pig*4) + ' legs together.' )
