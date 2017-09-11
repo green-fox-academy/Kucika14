787741e1c4f47a1932b1a6676b5cf7c763594aaa
Kucika14 <mate.akos@hotmail.com>
2017-09-11 14:39:12 +0200

    task 12 finihsed

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,11 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+height = 600
+width = 600
+length = 600
+
+surfaceArea = (height * width + width * length + length * height)
+volume = (height * width * length)
+print(surfaceArea)
+print(volume)
