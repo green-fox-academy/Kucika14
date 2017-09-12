52148d840766a1deb63a8533b8c3f2cc68923cd4
Kucika14 <mate.akos@hotmail.com>
2017-09-12 21:25:20 +0200

    reverse done(shorter list version)

--- week-02/day-02/reverse.py
+++ week-02/day-02/reverse.py
@@ -3,7 +3,7 @@
 # - Reverse the order of the elements in `aj`
 # - Print the elements of the reversed `aj`
 
-aj = [3, 4, 5, 6, 7,45,667,89,123]
+aj = [3, 4, 5, 6, 7]
 
 def reverse(text):
     output = []
