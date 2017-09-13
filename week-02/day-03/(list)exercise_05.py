0f32fbe340e8c1ed4712d1f897a64cbfa0498cb0
Kucika14 <mate.akos@hotmail.com>
2017-09-13 16:15:21 +0200

    (list)exercise_05

---
+++ week-02/day-03/(list)exercise_05.py
@@ -0,0 +1,15 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1, 2, 3, 4, 5, 6, 8];
+
+# for i in range(len(numbers)):
+#     if i == [7]:
+#         print("Hoorray")
+#     else:
+#         print("Noooooo")
+
+if 7 in numbers:
+    print("Hoorray")
+else:
+    print("Noooooo")
+    
