369a4faf92c49dc3963112bee8fcdee151b4d7a7
Kucika14 <mate.akos@hotmail.com>
2017-09-13 13:33:17 +0200

    (dictonarie)exercise_01.py halfway!

---
+++ week-02/day-03/(dictonarie)exercise_01.py
@@ -0,0 +1,23 @@
+students = [
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+]
+
+# print(students[0]'candies'])
+sum_candy = 0
+def candy_counter():
+    for i in range(len(students)):
+        sum_candy += (students[i]['candies'])
+        return sum_candy
+        # print(students[i]['candies'])
+
+print(candy_counter())
+# create a function that takes a list of students and prints: 
+# - how many candies are owned by students
+
+# create a function that takes a list of students and prints:
+# - Sum of the age of people who have lass than 5 candies
