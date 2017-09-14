f58d37e5c61d4bfb3949d625d2200f350710e500
Kucika14 <mate.akos@hotmail.com>
2017-09-14 17:29:06 +0200

    parametric_average.py (forgot to do)

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -9,9 +9,11 @@
 # The pyramid should have as many lines as the number was
 number = int(input('pls enter a number: '))
 output = "*"
-sapce = " "
+space = " "
 for i in range(1,number+1):
-    space = space * i
-    output = output * i
-    print(output)
+    space = space * (number -i)
+    output = output * (i * 2 -1)
+    star = space + output
+    print(star)
     output = "*"
+    space = " "
--- week-02/day-01/parametric_average.py
+++ week-02/day-01/parametric_average.py
@@ -4,3 +4,13 @@
 # integers like:
 #
 # Sum: 22, Average: 4.4
+
+
+number = int(input('pls enter a number: '))
+number1 = int(input('pls enter a second one: '))
+number2 = int(input('...and a third one :) : '))
+
+summ = number + number1 + number2
+average = summ / 3
+
+print(summ, average)
