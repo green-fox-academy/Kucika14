c8733a8790c6724981d3b3a8728626def9ee1496
Kucika14 <mate.akos@hotmail.com>
2017-09-14 10:04:27 +0200

    draw_square.py halfway

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,3 +10,12 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+number = int(input('pls enter a number: '))
+output = "%"
+space = " "
+for i in range(1,number+1):
+    output = number * output
+    space = number * (i + 1)
+    print(output)
+    output = "%"
+    space = " "
