d47d5bcc171228b498961092acb54b64933d7bb0
Kucika14 <mate.akos@hotmail.com>
2017-09-14 09:46:01 +0200

    draw_pyramid.py done

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -1,7 +1,6 @@
 # Write a program that reads a number from the standard input, then draws a
 # pyramid like this:
 #
-#1234567
 #    *
 #   ***
 #  *****
@@ -10,7 +9,9 @@
 # The pyramid should have as many lines as the number was
 number = int(input('pls enter a number: '))
 output = "*"
+sapce = " "
 for i in range(1,number+1):
+    space = space * i
     output = output * i
     print(output)
     output = "*"
