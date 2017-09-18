bc221ff3e4a0e23b050be2f9d4360eb0e8d120aa
Kucika14 <mate.akos@hotmail.com>
2017-09-18 17:36:56 +0200

    encoded_lines.py (just started)

---
+++ week-03/day-01/encoded_lines.py
@@ -0,0 +1,14 @@
+def decrypt(file_name):
+    with open(file, "r") as file1:
+    for line in file1:
+        chr(ord("a",-1))
+        print(i)
+
+
+
+
+
+
+
+file = 'E:/greenfox/Kucika14/week-03/day-01/encoded_lines.txt'
+decrypt(file)
