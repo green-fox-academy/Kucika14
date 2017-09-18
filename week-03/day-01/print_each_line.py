5a4da49e4569c4dbb5804bbeeeffa8bb01340798
Kucika14 <mate.akos@hotmail.com>
2017-09-18 12:01:19 +0200

    write_single_line.py

--- week-03/day-01/print_each_line.py
+++ week-03/day-01/print_each_line.py
@@ -2,6 +2,5 @@ try:
     my_file = open("my lines.txt", "r")
     for line in my_file:
         print(my_file.readline())
-        my_file.close
 except IOError:
     print("Unable to read file: my-file.txt")
---
+++ week-03/day-01/write_single_line.py
@@ -0,0 +1,11 @@
+# Open a file called "my-file.txt"
+# Write your name in it as a single line
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+
+try:
+    my_file = open("my-file.txt", "w")
+    my_file.write("Máté Ákos")
+    my_file.close
+except IOError:
+    print("Unable to write file: my-file.txt")
