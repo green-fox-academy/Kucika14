8e1365bd54a099142684603b33501dea943c73e9
Kucika14 <mate.akos@hotmail.com>
2017-09-18 11:53:25 +0200

    print_each_line.py(got a bit upgrade)

--- week-03/day-01/print_each_line.py
+++ week-03/day-01/print_each_line.py
@@ -2,5 +2,6 @@ try:
     my_file = open("my lines.txt", "r")
     for line in my_file:
         print(my_file.readline())
+        my_file.close
 except IOError:
     print("Unable to read file: my-file.txt")
