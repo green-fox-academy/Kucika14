1133ba3df0e3db456e78e6ab0a4d84b5d9873154
Kucika14 <mate.akos@hotmail.com>
2017-09-21 14:50:01 +0200

    string2.py done

--- week-03/day-04/string2.py
+++ week-03/day-04/string2.py
@@ -1,1 +1,14 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+
+
+def string(text):
+    if len(text) == 0:
+        return text
+    elif text[0] == "x":
+        return "" + string(text[1:])
+    else:
+        return text[0] + string(text[1:])
+
+
+print(string("fux kex box jax fax kox buxa xilofon"))
