1dad1814465d5f049b3b966509a45b98ed20f3f4
Kucika14 <mate.akos@hotmail.com>
2017-09-21 11:36:48 +0200

    power.py done

--- week-03/day-04/power.py
+++ week-03/day-04/power.py
@@ -1,2 +1,16 @@
 # Given base and n that are both 1 or more, compute recursively (no loops)
 # the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+
+def powerer(base, power):
+    if power == 1:
+        return base
+    else:
+        return base * powerer(base,power -1)
+
+
+
+
+
+
+print(powerer(3, 3))
