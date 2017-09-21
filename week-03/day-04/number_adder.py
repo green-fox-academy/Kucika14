4c8eab8390cbebccd67267bd58ceefbaa4c8ac4f
Kucika14 <mate.akos@hotmail.com>
2017-09-21 10:49:45 +0200

    number_adder.py done

--- week-03/day-04/number_adder.py
+++ week-03/day-04/number_adder.py
@@ -1,1 +1,11 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+
+def adder(num):
+    if num <= 1:
+        return num
+    else:
+        print(num)
+        return adder(num -1)+adder(num -2)
+        
+print(adder(10))
