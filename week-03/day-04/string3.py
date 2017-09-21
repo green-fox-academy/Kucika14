875827e651f3cf626d0505de73c33342948367e0
Kucika14 <mate.akos@hotmail.com>
2017-09-21 15:04:29 +0200

    string3.py done

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,13 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+def string(text):
+    text2 = ""
+    if len(text) == 1:
+        return text2 + text[0]
+    else:
+        text2 = text2 + text[0] + "*"
+        return text2 + string(text[1:])
+
+
+print(string("fux kex box"))
