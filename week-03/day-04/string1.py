632363c2e4247139c94ce0d5ac00f18b6cb9a13e
Kucika14 <mate.akos@hotmail.com>
2017-09-21 12:27:58 +0200

    string1.py (by Andras)

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,2 +1,14 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+
+def string(text):
+    if len(text) == 0:
+        return text
+    elif text[0] == "x":
+        return "y" + string(text[1:])
+    else:
+        return text[0] + string(text[1:])
+
+
+print(string("fux kex box"))
