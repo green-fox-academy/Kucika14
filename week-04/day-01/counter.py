0300ad308980bc9da25bf722f21f4a02ff7f79ac
Kucika14 <mate.akos@hotmail.com>
2017-09-25 21:19:31 +0200

    feat(counter.py) finished task

---
+++ week-04/day-01/counter.py
@@ -0,0 +1,20 @@
+class Counter(object):
+    def __init__(self, number=0):
+        self.number = number
+        self.init_number = number
+
+    def add(self, num=1):
+        self.number += num
+
+    def get(self):
+        return self.number
+    
+    def reset(self):
+        self.number = self.init_number
+
+counter = Counter(40)
+counter.add(5)
+counter.add()
+counter.add()
+counter.reset()
+print(counter.get())
