9fe921aeb39c181c04dfa77d24a6f2dfc31febb6
Kucika14 <mate.akos@hotmail.com>
2017-09-25 11:48:39 +0200

    refactor(post_it.py): refactored structure task

--- week-04/day-01/post_it.py
+++ week-04/day-01/post_it.py
@@ -1,12 +1,24 @@
 class PostIt(object):
-    background_color = ""
-    text = ""
-    text_color = ""
 
-orange = PostIt()
-PostIt2 = PostIt()
-PostIt3 = PostIt()
 
-orange.text = "idea1"
-pink.text = "awesome"
-yellow.text = "superb"
+    def __init__(self, background, text, text_color):
+        self.background = background
+        self.text = text
+        self.text_color = text_color
+
+
+orange = PostIt("orange", "Idea1", "blue")
+pink = PostIt("pink", "Awesome", "black" )
+yellow = PostIt("yellow", "Superb", "green")
+
+
+
+print(orange.background, orange.text, orange.text_color)
+print(pink.background, pink.text, pink.text_color)
+print(yellow.background, yellow.text, yellow.text_color)
+
+
+
+
+
+
