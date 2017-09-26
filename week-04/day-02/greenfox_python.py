66d56cb8df7bdb1ce9cd6384dfe8e48fbd93859b
Kucika14 <mate.akos@hotmail.com>
2017-09-26 11:13:22 +0200

    feat(greenfox_python.py) added Sponsor class

--- week-04/day-02/greenfox_python.py
+++ week-04/day-02/greenfox_python.py
@@ -12,13 +12,11 @@ class Person(object):
     def get_goal(self):
         print("My goal is: Live for the moment!")
 
-class Student(object):
+class Student(Person):
 
     def __init__(self, name='Jane Doe', age=30, gender='female',\
     previous_organization='The school of life', skipped_days=0):
-        self.name = name
-        self.age = age
-        self.gender = gender
+        super().__init__(name, age, gender)
         self.previous_organization = previous_organization
         self.skipped_days = skipped_days
 
@@ -32,12 +30,10 @@ class Student(object):
         str(self.skipped_days) + ' days from the course already.')
 
 
-class Mentor(object):
+class Mentor(Person):
 
     def __init__(self, name='Jane Doe', age=30, gender='female', level='intermediate'):
-        self.name = name
-        self.age = age
-        self.gender = gender
+        super().__init__(name, age, gender)
         self.level = level
 
 
@@ -49,14 +45,29 @@ class Mentor(object):
         print("Educate brilliant junior software developers.")
 
 
+class Sponsor(Person):
+    def __init__(self, name='Jane Doe', age=30, gender='female',\
+    company='Google', hired_students=0):
+        super().__init__(name, age, gender)
+        self.company = company
+        self.hired_students = hired_students
+
+    def hire(self):
+        self.hired_students += 1
+
+    def introduce(self):
+        print('Hi, I\'m ' + self.name + ' a ' + str(self.age) + ' year old ' + \
+        self.gender + ' who represents ' + self.company +' and hired ' + \
+        str(self.hired_students) +' students so far.')
+        return self
+
+    def get_goal(self):
+        print("Hire brilliant junior software developers.")
+
+class Pallida
 
 
 
-student = Student()
-student.get_goal().introduce()
-person = Person()
-person.name = 'Joci'
-mentor = Mentor()
-mentor.introduce().get_goal()
-person.introduce().get_goal()
-    
+sponsor = Sponsor()
+sponsor.hire()
+sponsor.introduce().get_goal()
