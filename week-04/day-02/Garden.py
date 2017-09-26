782ae9764b57e7082eed3ca50af441484542d35e
Kucika14 <mate.akos@hotmail.com>
2017-09-26 17:00:11 +0200

    feat(Garden.py) finished

--- week-04/day-02/Garden.py
+++ week-04/day-02/Garden.py
@@ -1,7 +1,9 @@
 class Garden(object):
-    def __init__(self, flower, tree):
-        self.flower = flower
-        self.tree = tree
+    def __init__(self):
+        self.plants = []
+
+    def plant_append(self, plant):
+        self.plants.append(plant)
         
     def water_lvl_check(self, flower_water_lvl, tree_water_lvl):
         if flower_water_lvl < 5 or tree_water_lvl < 10:
@@ -9,38 +11,63 @@ class Garden(object):
         else:
             flower_water_lvl() or tree_water_lvl
 
-    def watering_plants(self):
-        self.water += 3
+    def watering_plants(self):        
+        for plant in self.plants:
+            if isinstance(plant,Flower):
+                plant.water += 10*0.75
+            elif isinstance(plant,Tree):
+                plant.water += 10*0.4
+            plant.water_lvl()
 
-class Flower(Garden):
+class Plant(object):
     def __init__(self, color):
-        self.color = color
-        self.water = 1
+        self.water = 0
 
-    def flower_water_lvl(self):
+    
+
+class Tree(Plant):
+    def __init__(self, color):
+        super().__init__(color)
+        self.color = color
+
+    def water_lvl(self):
         if self.water <= 5:
+            print('The ' + self.color + ' tree needs water')
+
+        else:
+            print('This ' + self.color + ' tree is feeling good right now')
+
+
+class Flower(Plant):
+    def __init__(self, color):
+        super().__init__(color)
+        self.color = color
+
+
+    def water_lvl(self):
+        if self.water <= 10:
             print('The ' + self.color + ' flower needs water')
         else:
             print('This ' + self.color + ' flower is feeling good right now')
 
 
-class Tree(Garden):
-    def __init__(self, color):
-        self.color = color
-        self.water = 10
-
-    def tree_water_lvl(self):
-        if self.water <= 10:
-            print('The ' + self.color + ' tree needs water')
-        else:
-            print('This ' + self.color + ' tree is feeling good right now')
-
 
 flower = Flower('yellow')
+flower1 = Flower('blue')
 flower2 = Flower('pink')
-flower2.flower_water_lvl()
-flower2.watering_plants()
-flower2.watering_plants()
-flower2.watering_plants()
-flower2.flower_water_lvl()
+tree = Tree('Purple')
+tree1 = Tree('orange')
+tree2 = Tree('red')
+my_garden = Garden()
+my_garden.plant_append(flower)
+my_garden.plant_append(flower1)
+my_garden.plant_append(flower2)
+my_garden.plant_append(tree)
+my_garden.plant_append(tree1)
+my_garden.plant_append(tree2)
+my_garden.watering_plants()
+my_garden.watering_plants()
+my_garden.watering_plants()
+
+# print(plant.plants[0].color)
 
