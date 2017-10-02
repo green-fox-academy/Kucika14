1a09f460c336b983f80523bd65df29c3489ffa21
Kucika14 <mate.akos@hotmail.com>
2017-10-02 15:44:01 +0200

    feat(rpgame)hero moving

---
+++ week-05/rpg_game/rpg_gamecd/game.py
@@ -0,0 +1,4 @@
+from view import *
+my_map=Map()
+my_map.base_shape(0,0)
+my_map.display()
--- week-05/rpg_game/rpg_gamecd/view.py
+++ week-05/rpg_game/rpg_gamecd/view.py
@@ -9,8 +9,12 @@ class Map:
         self.canvas = Canvas(self.root, width=self.size+self.hud, height=self.size,bg="yellow",bd=0)
         self.floor = PhotoImage(file="floor.png")
         self.wall = PhotoImage(file="wall.png")
+        self.char = PhotoImage(file="hero-down.png")
         self.draw_map()
         self.canvas.pack()
+        self.root.bind("<KeyPress>", self.on_key_press)
+        self.canvas.focus_set()
+        
 
     def draw_map(self):
         size = 72
@@ -22,9 +26,30 @@ class Map:
                 if game_map_source[row][column]==1:
                     self.canvas.create_image(x+36, y+36, image=self.wall)
                 
+    def on_key_press(self, e):
+        if ( e.keysym == 'Up' ):
+            self.move(0,-10)
+        elif( e.keysym == 'Down' ):
+            self.move(0,10)
+        elif( e.keysym == 'Right'):
+            self.move(10,0)
+        elif( e.keysym == 'Left'):
+            self.move(-10,0)
+            
     def display(self):
-        self.draw_map()
         self.root.mainloop()
+
+    def base_shape(self, x, y):
+        self.hero = self.canvas.create_image(x+36, y+36, image=self.char)
+
+    def move(self, dx, dy):
+        self.canvas.move(self.hero, dx, dy )
+
+
+
+# Tell the canvas that we prepared a function that can deal with the key press events
+
+# Select the canvas to be in focused so it actually recieves the key hittings
 
 game_map_source = [
     [0,0,0,1,0,1,0,0,0,0],
@@ -38,6 +63,3 @@ game_map_source = [
     [0,1,1,1,0,0,0,0,1,0],
     [0,0,0,1,0,1,1,0,0,0]
 ]
-
-new_map = Map()
-new_map.display()
