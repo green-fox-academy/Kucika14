9d9c5f6bd184b191be5df42fefb30ad20fb92c84
Kucika14 <mate.akos@hotmail.com>
2017-10-04 10:56:33 +0200

    feat(rpgame)can't walk through walls

--- week-05/rpg_game/view.py
+++ week-05/rpg_game/view.py
@@ -13,6 +13,18 @@ class Map:
         self.char_up = PhotoImage(file="hero-up.png")
         self.char_right = PhotoImage(file="hero-right.png")
         self.char_left = PhotoImage(file="hero-left.png")
+        self.game_map_source = [
+            [0,0,0,1,0,1,0,0,0,0],
+            [0,0,0,1,0,1,0,1,1,0],
+            [0,1,1,1,0,1,0,1,1,0],
+            [0,0,0,0,0,1,0,0,0,0],
+            [1,1,1,1,0,1,1,1,1,0],
+            [0,1,0,1,0,0,0,0,1,0],
+            [0,1,0,1,0,1,1,0,1,0],
+            [0,0,0,0,0,1,1,0,1,0],
+            [0,1,1,1,0,0,0,0,1,0],
+            [0,0,0,1,0,1,1,0,0,0]
+        ]
         self.draw_map()
         self.canvas.pack()
         self.root.bind("<KeyPress>", self.on_key_press)
@@ -26,7 +38,7 @@ class Map:
                 x = column*size
                 y = row*size
                 self.canvas.create_image(x+36, y+36, image=self.floor)
-                if game_map_source[row][column]==1:
+                if self.game_map_source[row][column]==1:
                     self.canvas.create_image(x+36, y+36, image=self.wall)
                 
     def on_key_press(self, e):
@@ -34,19 +46,19 @@ class Map:
         self.canvas.delete(self.hero)
         if ( e.keysym == 'Up' ):
             self.hero = self.canvas.create_image(coords[0], coords[1], anchor=NW, image=self.char_up)
-            if coords[1] > 0:
+            if coords[1] > 0 and not self.get_wall_coords(coords[0], coords[1]-72) == 1:
                 self.move(0,-1)
         elif( e.keysym == 'Down' ):
             self.hero = self.canvas.create_image(coords[0], coords[1], anchor=NW, image=self.char_down)
-            if coords[1] < 648:
+            if coords[1] < 648 and not self.get_wall_coords(coords[0], coords[1]+72) == 1:
                 self.move(0,1)
         elif( e.keysym == 'Right'):
             self.hero = self.canvas.create_image(coords[0], coords[1], anchor=NW, image=self.char_right)
-            if coords[0] < 648:
+            if coords[0] < 648 and not self.get_wall_coords(coords[0]+72, coords[1]) == 1:
                 self.move(1,0)
         elif( e.keysym == 'Left'):
             self.hero = self.canvas.create_image(coords[0], coords[1], anchor=NW, image=self.char_left)
-            if coords[0] > 0:
+            if coords[0] > 0 and not self.get_wall_coords(coords[0]-72, coords[1]) == 1:
                 self.move(-1,0)
     
            
@@ -60,21 +72,12 @@ class Map:
     def move(self, dx, dy):
         self.canvas.move(self.hero, dx*72, dy*72)
 
-    def get_wall_coords
+    def get_wall_coords(self,x ,y):
+        x = int(x//72)
+        y = int(y//72)
+        return self.game_map_source[y][x]
+        
 
-# Tell the canvas that we prepared a function that can deal with the key press events
+        
 
-# Select the canvas to be in focused so it actually recieves the key hittings
 
-game_map_source = [
-    [0,0,0,1,0,1,0,0,0,0],
-    [0,0,0,1,0,1,0,1,1,0],
-    [0,1,1,1,0,1,0,1,1,0],
-    [0,0,0,0,0,1,0,0,0,0],
-    [1,1,1,1,0,1,1,1,1,0],
-    [0,1,0,1,0,0,0,0,1,0],
-    [0,1,0,1,0,1,1,0,1,0],
-    [0,0,0,0,0,1,1,0,1,0],
-    [0,1,1,1,0,0,0,0,1,0],
-    [0,0,0,1,0,1,1,0,0,0]
-]
