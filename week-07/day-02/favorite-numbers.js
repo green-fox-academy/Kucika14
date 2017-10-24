d1db5d449dc97fc65b0f07f786b687486774cc07
Kucika14 <mate.akos@hotmail.com>
2017-10-24 11:48:27 +0200

    feat(favorite-numbers) task done!

--- week-07/day-02/favorite-numbers.js
+++ week-07/day-02/favorite-numbers.js
@@ -2,6 +2,6 @@
 
 var favoriteNumber = 8
 // Store your favorite number in a variable (as a number)
-// And print it like this: 'My favorite number is: 8'
+// And print it like this: 'My favorite number is: 8' 
 
 console.log(favoriteNumber);
