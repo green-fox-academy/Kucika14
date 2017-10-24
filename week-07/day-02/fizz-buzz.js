6d7b24e21c942dcb2373c9cce310a11400799aba
Kucika14 <mate.akos@hotmail.com>
2017-10-24 17:00:41 +0200

    feat(fizz-buzz) task halfway!

---
+++ week-07/day-02/fizz-buzz.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+let outPut = ''
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        outPut = 'Fizz';
+    } else if (i % 5 === 0) {
+        outPut = 'Buzz';
+    } else if (i % 5 === 0 && i % 3 === 0); {
+        outPut = 'FizzBuzz';
+    }
+}
+
+console.log(outPut);
