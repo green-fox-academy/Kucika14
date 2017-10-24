bd704a005ca2922d6b8856d7f049a9920045451f
Kucika14 <mate.akos@hotmail.com>
2017-10-24 12:00:33 +0200

    feat(define-basic-info) task done!

---
+++ week-07/day-02/define-basic-info.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Ákos';
+let age = 26;
+let height = 1.80;
+let married = true;
