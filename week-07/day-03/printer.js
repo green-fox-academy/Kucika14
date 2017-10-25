a3d2e0da722573b4a9eafb29e0c3ebff9294cc8c
Kucika14 <mate.akos@hotmail.com>
2017-10-25 13:56:30 +0200

    feat(printer) task done!

---
+++ week-07/day-03/printer.js
@@ -0,0 +1,15 @@
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+
+
+let kecske = 'kecske';
+let malac = 'malac'
+let pocok = 'pocok'
+
+let printer = function(a,b,c) {
+    console.log(a + ' ' + b + ' ' + c);
+}
+
+printer(kecske,malac,pocok);
