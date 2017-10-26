d0304ee3814755ffe08966a34635f5e7184165fc
Kucika14 <mate.akos@hotmail.com>
2017-10-26 12:17:18 +0200

    feat(queryselector, query) task done!

---
+++ week-07/day-04/query.js
@@ -0,0 +1,23 @@
+'use stirct'
+
+let king = document.getElementById('b325');
+console.log('king', king)
+
+let conceited = document.getElementsByClassName('b326');
+alert(conceited);
+
+let businessLamp = document.getElementsByClassName('big')
+console.log('big', businessLamp);
+
+let conceitedKing = document.querySelectorAll('.container div');
+conceitedKing.forEach(function(e) {
+    alert(e);
+});
+
+let noBusiness = document.querySelectorAll('div');
+noBusiness.forEach(function(e) {
+    console.log(e);
+});
+
+let allBizniss = document.querySelectorAll('asteroid');
+alert(allBizniss);
