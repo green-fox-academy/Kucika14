a01e0d46e363a4bf15b534c665fdf1268fcbb439
Kucika14 <mate.akos@hotmail.com>
2017-10-30 16:15:16 +0100

    feat(animal-farm.js) progress method added

--- week-08/day-01/animal-farm.js
+++ week-08/day-01/animal-farm.js
@@ -14,13 +14,38 @@ function Animal() {
 }
 
 
-function Farm() {
+function Farm(slots) {
     this.slots = slots;
     this.animal = [];
     for (let i = 0; i < slots; i++) {
         this.animal.push(new  Animal());
     }
     this.breed = function() {
-        if
+        if (this.animal.length < this.slots) {
+            this.animal.push(new  Animal());
+        }
+    }
+    this.slaughter = function() {
+
+    }
+    this.progress = function() {
+        this.animal.forEach(function(e){
+            let randomList = [];
+            let randomNumber = Math.random();
+            for (let i = 0; i < 3; i++) {
+                randomList.push(randomList);
+            }
+            if (randomList[0] > 0.5) {
+                Animal.eat();
+            } else if ( randomList[1] > 0.5) {
+                Animal.drink();
+            } else (randomList[2] > 0.5) {
+                Animal.play();
+            }
+        })
     }
 }
+
+let farm = Farm(20)
+
+farm.breed
