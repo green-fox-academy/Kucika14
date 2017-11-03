618727d27105d50a2ac209119cdc36978caf9e9b
Kucika14 <mate.akos@hotmail.com>
2017-11-03 21:52:33 +0100

    feat(reddit) list elements added!

---
+++ week-08/day-05/reddit.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let ul = document.querySelector('ul')
+        
+let doRequest = function(callback) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://secure-reddit.herokuapp.com/simple/posts');
+    x.onload = function() {
+        callback(x.responseText)
+    };
+    x.send(null);
+}
+
+function handleData(data){
+    let wholeReddit = JSON.parse(data);
+    let reddit = wholeReddit.posts;
+    console.log(reddit) 
+    reddit.forEach(function(e) {
+        let link = document.createElement('a');
+        ul.appendChild(link);
+        link.setAttribute('href', e.url);
+        link.textContent = e.title;
+        
+
+    })
+    
+}
+
+doRequest(handleData)
